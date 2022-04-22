4f705b226b410a8b14c6ca6254b94a8370ed514d
oscar <oscarht78@hotmail.com>
2022-04-22 11:21:08 -0700

    se añade texto de aclaración feat: fix

---
+++ Ejemplo2/Ejemplo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ejemplo2
+//
+//  Created by Alumno Swift on 22/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello World")
+    }
+
+
+}
+
